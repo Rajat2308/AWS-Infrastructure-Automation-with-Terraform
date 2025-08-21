133c32dbf9fe03177dadeab31a6c249e8d49f4d2
Rajat joshi <111409270+Rajat2308@users.noreply.github.com>
2025-08-21 21:04:21 +0530

    Create securitygroup.tf

---
+++ securitygroup.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "mygroup" {
+  name        = "mysg"
+  description = "This is a security group created by Terraform."
+  vpc_id      = aws_default_vpc.default.id
+
+  tags = {
+    Name = "mysg"
+  }
+
+  # Ingress (Inbound rules)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Egress (Outbound rules)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
