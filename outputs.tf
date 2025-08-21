f3990086e21c48651bef241a7f9149271ddeea94
Rajat joshi <111409270+Rajat2308@users.noreply.github.com>
2025-08-21 21:08:29 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "instance_public_ip" {
+  description = "Public IPs of the EC2 instances"
+  value       = aws_instance.my_instance[*].public_ip
+}
+
+output "instance_private_ip" {
+  description = "Private IPs of the EC2 instances"
+  value       = aws_instance.my_instance[*].private_ip
+}
