86d0a5341119bc19fa460ffda9a44b7d3ad9011f
Rajat joshi <111409270+Rajat2308@users.noreply.github.com>
2025-08-22 10:35:07 +0530

    Create local.tf

---
+++ local.tf
@@ -0,0 +1,20 @@
+locals {
+  instances = {
+    myinstance1 = {
+      instance_type     = "t2.micro"
+      availability_zone = "ap-east-1a"
+      name_tag          = "InstanceOne"
+    }
+    myinstance2 = {
+      instance_type     = "t2.medium"
+      availability_zone = "ap-east-1b"
+      name_tag          = "InstanceTwo"
+    }
+  }
+}
+
+
+for_each = local.instances
+
+  instance_type        = each.value.instance_type
+  availability_zone    = each.value.availability_zone
