a806b0e0a4a0f02e54444a2a454964e88822d9c9
Rajat joshi <111409270+Rajat2308@users.noreply.github.com>
2025-08-26 13:49:47 +0530

    Create conditionalExperssion.tf

---
+++ conditionalExperssion.tf
@@ -0,0 +1,14 @@
+# conditional Experssion
+
+variable env{
+	
+	name = "Pro'
+	type = string
+}
+
+root_block_device {
+	
+	#  volume_size = var.env == "Pro" ? <True> : <false>  ?= if := else
+
+	volume_size = var.env == "Pro" ? 20 : var.root_volume_size
+}
